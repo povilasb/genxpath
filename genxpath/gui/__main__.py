37f2d40c3ba4eef75b71db4380d14953654c136b
Povilas Balciunas <balciunas90@gmail.com>
2025-09-14 13:48:59 +0300

    gui: implement minimize xpath

--- genxpath/gui/__main__.py
+++ genxpath/gui/__main__.py
@@ -12,11 +12,12 @@ from textual.widgets import (
 )
 from textual.message import Message
 from textual.containers import VerticalScroll, Horizontal, Vertical
+from textual.reactive import reactive
 from cache3 import DiskCache
 from parsel import Selector
 
 from genxpath._io import http_get
-from genxpath._gen import find_xpaths_for
+from genxpath._gen import find_xpaths_for, minimize_xpath
 
 
 class FindValueInput(Input):
@@ -30,7 +31,7 @@ class FindValueInput(Input):
     """
 
 
-class QueryXpath(Horizontal):
+class QueryXpath(Static):
     BORDER_TITLE = "Query xpath"
     DEFAULT_CSS = """
     QueryXpath {
@@ -40,8 +41,47 @@ class QueryXpath(Horizontal):
     }
     """
 
+    doc: reactive[Selector | None] = reactive(None)
+
+    class SelectedHtmlElements(Message):
+        def __init__(self, elements: list[Selector]):
+            self.elements = elements
+            super().__init__()
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            yield Button("Minimize", tooltip="Find a shorter XPath")
+            yield Input(placeholder="//*[@id='product']")
+
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        if not self.doc:
+            self.notify("No document loaded")
+            return
+
+        xpath = event.value
+        try:
+            elements = self.doc.xpath(xpath)
+            self.post_message(self.SelectedHtmlElements(elements))
+        except ValueError:
+            self.notify("Invalid XPath: " + xpath, markup=False)
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if not self.doc:
+            self.notify("No document loaded")
+            return
+
+        curr_xpath = self.query_one(Input).value
+        if not curr_xpath:
+            self.notify("No XPath provided", severity="warning")
+            return
+
+        min_xpath = minimize_xpath(self.doc, curr_xpath)
+        self.query_one(Input).value = min_xpath
+
 
 class Controls(Static):
+    loaded_doc: reactive[Selector | None] = reactive(None)
+
     class LoadingUrl(Message):
         def __init__(self, url: str):
             self.url = url
@@ -50,11 +90,6 @@ class Controls(Static):
     class LoadedHtml(Message):
         def __init__(self, html: str):
             self.html = html
-            super().__init__()
-
-    class SelectedHtmlElements(Message):
-        def __init__(self, elements: list[Selector]):
-            self.elements = elements
             super().__init__()
 
     class FoundXpaths(Message):
@@ -66,31 +101,31 @@ class Controls(Static):
         super().__init__(*args, **kwargs)
 
         self._cache = cache
-        self._loaded_doc: Selector | None = None
 
     def compose(self) -> ComposeResult:
-        yield Input(
-            value=self._cache.get("last_url_loaded"),
-            placeholder="URL or file path",
-            id="url-input",
-        )
+        self._url_input = Input(placeholder="URL or file path", id="url-input")
+        yield self._url_input
         yield FindValueInput(
             placeholder="Sample text in the document", id="value-input"
         )
-        yield QueryXpath(
-            Button("Minimize", tooltip="Find a shorter XPath"),
-            Input(placeholder="//*[@id='product']", id="xpath-input"),
-        )
+        yield QueryXpath()
+
+    def on_mount(self) -> None:
+        # If it stays focused, for some reasons some random characters are inserted
+        self._url_input.blur()
+
+        if last_url := self._cache.get("last_url_loaded"):
+            self._url_input.value = last_url
+            self._fetch_html(last_url)
+
+    def watch_loaded_doc(self, loaded_doc: Selector | None) -> None:
+        self.query_one(QueryXpath).doc = loaded_doc
 
     def on_input_submitted(self, event: Input.Submitted) -> None:
         if event.input.id == "url-input":
             self._fetch_html(event.value)
-        elif event.input.id == "xpath-input" and event.value:
-            self._query_xpath(event.value)
         elif event.input.id == "value-input" and event.value:
-            self._query_xpath_for_value(event.value)
-
-    # TODO: minimize xpath from full path
+            self._find_xpaths(event.value)
 
     def _fetch_html(self, url: str) -> None:
         self.post_message(self.LoadingUrl(url))
@@ -101,26 +136,17 @@ class Controls(Static):
             self.notify("Error fetching HTML: " + str(e), markup=False)
             return
 
+        self.loaded_doc = Selector(text=html_doc)
         self.post_message(self.LoadedHtml(html_doc))
-        self._loaded_doc = Selector(text=html_doc)
 
         self._cache.set("last_url_loaded", url)
 
-    def _query_xpath(self, xpath: str) -> None:
-        if not self._loaded_doc:
+    def _find_xpaths(self, value: str) -> None:
+        if not self.loaded_doc:
+            self.notify("No document loaded")
             return
 
-        try:
-            elements = self._loaded_doc.xpath(xpath)
-            self.post_message(self.SelectedHtmlElements(elements))
-        except ValueError:
-            self.notify("Invalid XPath: " + xpath, markup=False)
-
-    def _query_xpath_for_value(self, value: str) -> None:
-        if not self._loaded_doc:
-            return
-
-        xpaths = find_xpaths_for(value, self._loaded_doc)
+        xpaths = find_xpaths_for(value, self.loaded_doc)
         self.post_message(self.FoundXpaths(xpaths))
 
 
@@ -204,8 +230,7 @@ class XpathGenerator(App):
         super().__init__(*args, **kwargs)
 
         self._cache = DiskCache("cache")
-        self.theme = "textual-light"
-        self._loaded_doc: Selector | None = None
+        self.theme = "solarized-light"
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -215,10 +240,6 @@ class XpathGenerator(App):
                 yield Controls(self._cache)
                 yield ViewHtml()
 
-            # with TabPane("Find values"):
-            #     yield Input(placeholder="Value", id="find-value-input")
-            #     yield DataTable(name="XPaths")
-
         yield Footer()
 
     def on_controls_loading_url(self, event: Controls.LoadingUrl) -> None:
@@ -227,13 +248,13 @@ class XpathGenerator(App):
     def on_controls_loaded_html(self, event: Controls.LoadedHtml) -> None:
         self.query_one(ViewHtml).update_html(f"```html\n{event.html}\n```")
 
-    def on_controls_selected_html_elements(
-        self, event: Controls.SelectedHtmlElements
-    ) -> None:
-        self.query_one(ViewHtml).list_html_elements(event.elements)
-
     def on_controls_found_xpaths(self, event: Controls.FoundXpaths) -> None:
         self.query_one(ViewHtml).list_xpaths(event.xpaths)
+
+    def on_query_xpath_selected_html_elements(
+        self, event: QueryXpath.SelectedHtmlElements
+    ) -> None:
+        self.query_one(ViewHtml).list_html_elements(event.elements)
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
